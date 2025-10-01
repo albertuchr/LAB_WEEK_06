028d81c5390d802521a95740ff075bd0fbfa0918
albertuchr <albertuschristianto27@gmail.com>
2025-10-01 11:17:33 +0700

    Commit No.2 - Clickable RecyclerView

--- app/src/main/java/com/example/lab_week_06/CatAdapter.kt
+++ app/src/main/java/com/example/lab_week_06/CatAdapter.kt
@@ -4,31 +4,40 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.lab_week_06.model.CatModel
 import android.view.LayoutInflater
 import android.view.ViewGroup
-class CatAdapter(private val layoutInflater: LayoutInflater, private val
-imageLoader: ImageLoader) : RecyclerView.Adapter<CatViewHolder>() {
-    //Mutable list for storing all the list data
+
+class CatAdapter(
+    private val layoutInflater: LayoutInflater,
+    private val imageLoader: ImageLoader,
+    private val onClickListener: OnClickListener
+) : RecyclerView.Adapter<CatViewHolder>() {
+
     private val cats = mutableListOf<CatModel>()
-    //A function to set the mutable list
+
     fun setData(newCats: List<CatModel>) {
         cats.clear()
         cats.addAll(newCats)
-//This is used to tell the adapter that there's a data change in the
+        notifyDataSetChanged()
+    }
 
-                notifyDataSetChanged()
-    }
-    //A view holder is used to bind the data to the layout views
-//onCreateViewHolder is instantiating the view holder it self
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int):
-            CatViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CatViewHolder {
         val view = layoutInflater.inflate(R.layout.item_list, parent, false)
-        return CatViewHolder(view, imageLoader)
+
+        // terjemahkan onClick (ViewHolder) → onItemClick (Adapter)
+        return CatViewHolder(view, imageLoader, object : CatViewHolder.OnClickListener {
+            override fun onClick(cat: CatModel) {
+                onClickListener.onItemClick(cat)
+            }
+        })
     }
-    //This is used to get the amount of data/item in the list
+
     override fun getItemCount() = cats.size
-    //This is used to bind each data to each layout views
+
     override fun onBindViewHolder(holder: CatViewHolder, position: Int) {
-//The holder parameter stores our previously created ViewHolder
-//The holder.bindData function is declared in the CatViewHolder
         holder.bindData(cats[position])
+    }
+
+    // Interface untuk Activity
+    interface OnClickListener {
+        fun onItemClick(cat: CatModel)
     }
 }
--- app/src/main/java/com/example/lab_week_06/CatViewHolder.kt
+++ app/src/main/java/com/example/lab_week_06/CatViewHolder.kt
@@ -10,8 +10,9 @@ import com.example.lab_week_06.model.Gender
 private val FEMALE_SYMBOL = "\u2640"
 private val MALE_SYMBOL = "\u2642"
 private const val UNKNOWN_SYMBOL = "?"
-class CatViewHolder(containerView: View, private val imageLoader:
-ImageLoader) : RecyclerView.ViewHolder(containerView) {
+class CatViewHolder(private val containerView: View, private val
+imageLoader: ImageLoader, private val onClickListener: OnClickListener) :
+    RecyclerView.ViewHolder(containerView) {
 //containerView is the container layout of each item list
 //Here findViewById is used to get the reference of each views inside
 
@@ -27,6 +28,11 @@ ImageLoader) : RecyclerView.ViewHolder(containerView) {
         containerView.findViewById(R.id.cat_photo) }
     //This function is called in the adapter to provide the binding function
     fun bindData(cat: CatModel) {
+//Override the onClickListener function
+        containerView.setOnClickListener {
+//Here we are using the onClickListener passed from the Adapter
+            onClickListener.onClick(cat)
+        }
         imageLoader.loadImage(cat.imageUrl, catPhotoView)
         catNameView.text = cat.name
         catBreedView.text = when (cat.breed) {
@@ -41,5 +47,9 @@ ImageLoader) : RecyclerView.ViewHolder(containerView) {
             Gender.Male -> MALE_SYMBOL
             else -> UNKNOWN_SYMBOL
         }
+    }
+    //Declare an onClickListener interface
+    interface OnClickListener {
+        fun onClick(cat: CatModel)
     }
 }
